ded4396fd50591788acd91cd42d4b80810c0643d
Artem Bondarchuk <artem@undock.com>
2022-05-20 16:02:55 +0800

    104%

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.use(helmet());
 
