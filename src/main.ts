3fbf089b558a4173cfbce4733cf09605b9e615b2
Artem Bondarchuk <artem@undock.com>
2022-05-20 15:44:45 +0800

    102%

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,26 @@ async function bootstrap() {
 
   app.use(helmet());
 
+  const whitelist = [
+    'http://localhost:4200/',
+    'https://pink-unicorn-sourcing-form.web.app/',
+  ];
   app.enableCors({
-    origin: [
-      'http://localhost:4200/',
-      'https://pink-unicorn-sourcing-form.web.app/',
-    ],
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
   });
+
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT || 8080);
