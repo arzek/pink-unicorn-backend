3aa31561d5eeffe45040b770ab54458140f18638
Artem Bondarchuk <artem@undock.com>
2022-05-20 17:48:42 +0800

    106%

--- src/form.dto.ts
+++ src/form.dto.ts
@@ -1,40 +1,52 @@
 import { IsEmail, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class FormDto {
+  @IsOptional()
   @IsString()
   firstName: string;
 
+  @IsOptional()
   @IsString()
   lastName: string;
 
+  @IsOptional()
   @IsString()
   @IsEmail()
   email: string;
 
+  @IsOptional()
   @IsString()
   phone: string;
 
+  @IsOptional()
   @IsString()
   country: string;
 
+  @IsOptional()
   @IsString()
   city: string;
 
+  @IsOptional()
   @IsString()
   desiredPosition: string;
 
+  @IsOptional()
   @IsString()
   skills: string;
 
+  @IsOptional()
   @IsNumber()
   experience: number;
 
+  @IsOptional()
   @IsString()
   english: string;
 
+  @IsOptional()
   @IsNumber()
   salary: number;
 
+  @IsOptional()
   @IsString()
   communicationChannel: string;
 
@@ -58,6 +70,7 @@ export class FormDto {
   @IsString()
   comment: string;
 
+  @IsOptional()
   @IsString()
   cv: string;
 }
